874e54d56b06140631cbcd4f20c1a4b166d23401
Johanne <johanne_lewis@hotmail.com>
2019-08-21 23:59:16 +0100

    fix bug in code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading.js';
 import addImage from './add-image';
-import { EnvironmentPlugin } from 'webpack';
 
 const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
@@ -11,9 +10,9 @@ helloWorldButton.render();
 
 addImage();
 
-if(process.EnvironmentPlugin.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     console.log('production mode');
 }
-else if(process.EnvironmentPlugin.NODE_ENV === 'development'){
+else if(process.env.NODE_ENV === 'development'){
     console.log('development mode');
 }
