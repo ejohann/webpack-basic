cd90389bf7d8630a2ad8a6a194296dbfdcd53bd5
Johanne <johanne_lewis@hotmail.com>
2019-08-21 10:25:32 +0100

    Import and instantiate heading class, render heading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading.js';
 import addImage from './add-image';
 
+const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
+
+heading.render();
 helloWorldButton.render();
 
 //addImage();
