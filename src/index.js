c93a7bad56c81f6d4d33de4c976acbbfdc89c5f0
Johanne <johanne_lewis@hotmail.com>
2019-08-21 23:48:37 +0100

    Log mode to the console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading.js';
 import addImage from './add-image';
+import { EnvironmentPlugin } from 'webpack';
 
 const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
@@ -9,3 +10,10 @@ heading.render();
 helloWorldButton.render();
 
 addImage();
+
+if(process.EnvironmentPlugin.NODE_ENV === 'production'){
+    console.log('production mode');
+}
+else if(process.EnvironmentPlugin.NODE_ENV === 'development'){
+    console.log('development mode');
+}
