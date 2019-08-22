e784a3762a65da92cfe35a5a737d5b0d6a59d93f
Johanne <johanne_lewis@hotmail.com>
2019-08-22 10:33:31 +0100

    Updated configuration file for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -52,7 +51,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new TerserPlugin(),
         new MiniCssExtractPlugin({
             filename: 'styles.[contenthash].css'
         }),
